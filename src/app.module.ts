f4e94c7a8bca8381458950797293e57949c193e0
unknown <ladjrafi_zakaria@yahoo.com>
2025-03-16 19:37:59 +0100

    added Auth module to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 // import { AppService } from './app.service';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { UserModule } from './modules/users/users.module';
+import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
+// import { AuthModule } from './modules/auth/auth.module';
 // import { AppResolver } from './app.resolver';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true, // Active GraphQL Playground
+      introspection: true, // Permet l’inspection du schéma
     }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
