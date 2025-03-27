0c0c3134115a618f2d223b795e737d11b7559f53
unknown <ladjrafi_zakaria@yahoo.com>
2025-03-27 21:35:39 +0100

    injected providers and exports in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthRepository } from './auth.repository';
 import { AuthResolver } from './auth.resolver';
-
+import { CreateUserUseCase } from './useCase/createUseUseCase';
+import { UserRepository } from '../users/users.repository';
+import { LoginUserUseCase } from './useCase/loginUserUseCase';
 @Module({
   imports: [],
-  providers: [AuthRepository, AuthResolver, PrismaService],
-  exports: [AuthRepository],
+
+  providers: [
+    AuthRepository,
+    AuthResolver,
+    PrismaService,
+    CreateUserUseCase,
+    UserRepository,
+    LoginUserUseCase,
+  ],
+  exports: [AuthRepository, CreateUserUseCase, LoginUserUseCase],
 })
 export class AuthModule {}
