ec8e22d7b08250231d6f92cc1749cef46c53ccee
unknown <ladjrafi_zakaria@yahoo.com>
2025-04-06 21:18:21 +0200

    instanciated AuthDTO to respect definition of DTO

--- src/modules/auth/useCase/loginUserUseCase.ts
+++ src/modules/auth/useCase/loginUserUseCase.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { AuthRepository } from '../auth.repository';
 import bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
+import { AuthDTO } from '../dto/Auth.dto';
 
 @Injectable()
 export class LoginUserUseCase {
@@ -19,12 +20,20 @@ export class LoginUserUseCase {
     }
 
     try {
-      const accessToken = jwt.sign({ userId: user.id }, 'ACCESS_SECRET', {
-        expiresIn: '15m',
-      });
-      const refreshToken = jwt.sign({ userId: user.id }, 'REFRESH_SECRET', {
-        expiresIn: '7d',
-      });
+      const accessToken = jwt.sign(
+        { userId: user.id },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: '15m',
+        },
+      );
+      const refreshToken = jwt.sign(
+        { userId: user.id },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: '7d',
+        },
+      );
 
       await this.authrepository.updateTokens(
         user.id,
@@ -32,11 +41,22 @@ export class LoginUserUseCase {
         accessToken,
       );
 
-      return {
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-        user,
-      };
+      const authDTO = new AuthDTO();
+      authDTO.id = user.auth.id;
+      authDTO.userId = user.auth.userId;
+      authDTO.refreshToken = user.auth.refreshToken;
+      authDTO.accessToken = user.auth.accessToken;
+      authDTO.updatedAt = user.auth.updatedAt;
+      authDTO.createdAt = user.auth.createdAt;
+      authDTO.user = user;
+
+      return authDTO;
+
+      // return {
+      //   accessToken: accessToken,
+      //   refreshToken: refreshToken,
+      //   user,
+      // };
     } catch (error: unknown) {
       if (error instanceof Error) {
         console.error(error.message);
