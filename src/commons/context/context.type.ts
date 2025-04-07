79b6f89a83b24cf37f32dd6cd032957ef541d10e
unknown <ladjrafi_zakaria@yahoo.com>
2025-04-07 19:52:49 +0200

    added definition of property user of type JwtPayloadin GqlContext

--- src/commons/context/context.type.ts
+++ src/commons/context/context.type.ts
@@ -1,7 +1,9 @@
 import { Request } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 
 export interface GqlContext {
   req: RequestWithUser;
+  user: JwtPayload;
 }
 
 export interface RequestWithUser extends Request {
